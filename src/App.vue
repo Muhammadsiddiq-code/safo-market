68730b72f9108c7b20b0b34a9b2609ac61adf7b2
Muhammadsiddiq-code <xamidullayevich10@gmail.com>
2025-07-04 15:46:22 +0500

    add

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@
       <div class="form-wrapper">
         <div class="logo-section">
           <div class="logo-icon">
-            <svg class="logo-svg" fill="currentColor" viewBox="0 0 24 24">
+            <!-- <svg class="logo-svg" fill="currentColor" viewBox="0 0 24 24">
               <path d="M7 4V2C7 1.45 7.45 1 8 1H16C16.55 1 17 1.45 17 2V4H20C20.55 4 21 4.45 21 5S20.55 6 20 6H19V19C19 20.1 18.1 21 17 21H7C5.9 21 5 20.1 5 19V6H4C3.45 6 3 5.55 3 5S3.45 4 4 4H7ZM9 3V4H15V3H9ZM7 6V19H17V6H7Z"/>
-            </svg>
+            </svg> -->
+            <img  src="../src/assets/image/Annotation 2025-06-25 170628.png" alt="">
+
+            
           </div>
-          <div class="logo-text">
+          <!-- <div class="logo-text">
             <div class="logo-title">SAFO</div>
             <div class="logo-subtitle">MARKET</div>
-          </div>
+          </div> -->
         </div>
 
         <div class="welcome-section">
@@ -84,16 +87,26 @@ const togglePassword = () => {
   showPassword.value = !showPassword.value
 }
 
-const handleLogin = () => {
-  if (loginData.username && loginData.password) {
-    console.log('Login attempt:', {
-      username: loginData.username,
-      password: loginData.password
-    })
+// const handleLogin = () => {
+//   if (loginData.username && loginData.password) {
+//     console.log('Login attempt:', {
+//       username: loginData.username,
+//       password: loginData.password
+//     })
     
-    alert(`Kirish urinishi:\nLogin: ${loginData.username}\nParol: ${loginData.password}`)
+//     alert(`Kirish urinishi:\nLogin: ${loginData.username}\nParol: ${loginData.password}`)
+//   }
+// }
+
+
+const handleLogin = () => {
+  if (loginData.username === "admin" && loginData.password === "1234") {
+    window.location.href = 'https://exam-vue-js.vercel.app/';
+  } else {
+    alert("Login yoki parol hato");
   }
 }
+
 
 
 
@@ -142,6 +155,7 @@ const handleLogin = () => {
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-left: 40px;
 }
 
 .logo-svg {
